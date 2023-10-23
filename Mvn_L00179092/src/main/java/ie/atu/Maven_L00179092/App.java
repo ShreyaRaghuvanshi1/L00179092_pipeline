ed067846e90b2178983dcdf21d16b41c273262e4
ShreyaRaghuvanshi1 <L00179092@atu.ie>
2023-10-23 20:48:08 +0100

    new files added

---
+++ Mvn_L00179092/src/main/java/ie/atu/Maven_L00179092/App.java
@@ -0,0 +1,13 @@
+package ie.atu.Maven_L00179092;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mvn_L00179092/src/test/java/ie/atu/Maven_L00179092/AppTest.java
@@ -0,0 +1,20 @@
+package ie.atu.Maven_L00179092;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
