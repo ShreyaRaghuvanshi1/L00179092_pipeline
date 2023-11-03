b286ed726c65b008ff6584949a17e60f0ebc98c0
ShreyaRaghuvanshi1 <145033750+ShreyaRaghuvanshi1@users.noreply.github.com>
2023-11-03 14:40:09 +0000

    Update App.java

--- Mvn_L00179092/src/main/java/ie/atu/Maven_L00179092/App.java
+++ Mvn_L00179092/src/main/java/ie/atu/Maven_L00179092/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        log.debug( "Hello World, I am Shreya Raghuvanshi" );
     }
 }
